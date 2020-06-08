8cb55ec108131133ef508ddb449ea85be1aee287
Aquiles Carattino <aqui.carattino@gmail.com>
2020-06-08 17:35:45 +0200

    Added simple program to interact only with Arduino

---
+++ electronics/models/experiment.py
@@ -0,0 +1,48 @@
+import yaml
+
+from dispertech.models.electronics.arduino import ArduinoModel
+from experimentor.models.experiments import Experiment
+
+
+class ControlArduino(Experiment):
+    def __init__(self, filename=None):
+        super().__init__()
+        self.load_configuration(filename, loader=yaml.UnsafeLoader)
+        self.electronics = None
+        self.servo = 0
+
+    def initialize(self):
+        self.electronics = ArduinoModel(self.config['electronics']['arduino']['device'])
+        self.electronics.initialize()
+
+    def toggle_side_led(self):
+        self.electronics.side_led = 0 if self.electronics.side_led else 1
+
+    def toggle_top_led(self):
+        self.electronics.top_led = 0 if self.electronics.top_led else 1
+
+    def toggle_fiber_led(self):
+        self.electronics.fiber_led = 0 if self.electronics.fiber_led else 1
+
+    def toggle_power_led(self):
+        self.electronics.power_led = 0 if self.electronics.power_led else 1
+
+    def toggle_laser_led(self):
+        self.electronics.laser_led = 0 if self.electronics.laser_led else 1
+
+    def toggle_measure_led(self):
+        self.electronics.measure_led = 0 if self.electronics.measure_led else 1
+
+    def laser_power(self, power):
+        self.electronics.laser_power(power)
+
+    def toggle_servo(self):
+        if self.servo:
+            self.electronics.move_servo(0)
+            self.servo = 0
+        else:
+            self.electronics.move_servo(1)
+            self.servo = 1
+
+    def move_mirror(self, speed, direction, axis):
+        self.electronics.move_mirror(speed, direction, axis)
---
+++ electronics/view/electronics_window.py
@@ -0,0 +1,46 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow
+
+import pathlib
+
+
+class ElectronicsWindow(QMainWindow):
+    def __init__(self, experiment):
+        super().__init__(parent=None)
+        curr_path = pathlib.Path(__file__).parent
+        uic.loadUi(curr_path/'GUI'/'electronics_window.ui', self)
+        self.experiment = experiment
+
+        self.side_led_button.clicked.connect(self.experiment.toggle_side_led)
+        self.top_led_button.clicked.connect(self.experiment.toggle_top_led)
+        self.fiber_led_button.clicked.connect(self.experiment.toggle_fiber_led)
+
+        self.servo_button.clicked.connect(self.experiment.toggle_servo)
+
+        self.laser_power_slider.valueChanged.connect(self.update_power)
+
+        self.button_up.clicked.connect(self.move_up)
+        self.button_down.clicked.connect(self.move_down)
+        self.button_left.clicked.connect(self.move_left)
+        self.button_right.clicked.connect(self.move_right)
+
+    def update_power(self, power):
+        power = int(power)
+        self.laser_power_lcd.display(power)
+        self.experiment.laser_power(power)
+
+    def move_right(self):
+        speed = int(self.mirror_speed.text())
+        self.experiment.move_mirror(speed=speed, direction=1, axis=1)
+
+    def move_left(self):
+        speed = int(self.mirror_speed.text())
+        self.experiment.move_mirror(speed=speed, direction=0, axis=1)
+
+    def move_up(self):
+        speed = int(self.mirror_speed.text())
+        self.experiment.move_mirror(speed=speed, direction=1, axis=2)
+
+    def move_down(self):
+        speed = int(self.mirror_speed.text())
+        self.experiment.move_mirror(speed=speed, direction=0, axis=2)
