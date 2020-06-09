3c1bbf9be9c3a7dc4ccdbd1dfd7d1190a7d2dec0
Aquiles Carattino <aqui.carattino@gmail.com>
2020-06-09 15:33:26 +0200

    Swapped axes for the piezo mirror

--- calibration/view/microscope_window.py
+++ calibration/view/microscope_window.py
@@ -124,16 +124,16 @@ class MicroscopeWindow(QMainWindow):
             self.button_laser_status = 1
 
     def move_right(self):
-        self.experiment.move_mirror(direction=1, axis=2)
-
-    def move_left(self):
-        self.experiment.move_mirror(direction=0, axis=2)
-
-    def move_up(self):
         self.experiment.move_mirror(direction=1, axis=1)
 
-    def move_down(self):
+    def move_left(self):
         self.experiment.move_mirror(direction=0, axis=1)
+
+    def move_up(self):
+        self.experiment.move_mirror(direction=1, axis=2)
+
+    def move_down(self):
+        self.experiment.move_mirror(direction=0, axis=2)
 
     def update_image(self):
         if self.background_box.isChecked() and self.experiment.background is not None:
