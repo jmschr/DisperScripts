e24b9a7ca0da402b4bca81812860dae2e1d98dd8
Aquiles Carattino <aqui.carattino@gmail.com>
2021-07-06 18:07:10 +0200

    Added stylesheet to window

--- start_piezo_move.py
+++ start_piezo_move.py
@@ -1,3 +1,6 @@
+import os
+
+import qdarkstyle
 import sys
 
 
@@ -6,6 +9,8 @@ from PyQt5.QtWidgets import QApplication
 from PiezoMove.arduino import ArduinoModel
 from PiezoMove.main_window import PiezoMoveWindow
 
+os.environ['QT_API'] = 'pyqt5'
+
 device = 5
 
 arduino = ArduinoModel(device=device)
@@ -13,5 +18,8 @@ arduino.initialize()
 
 app = QApplication([])
 win = PiezoMoveWindow(arduino)
+
+app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt5'))
+
 win.show()
 sys.exit(app.exec())
