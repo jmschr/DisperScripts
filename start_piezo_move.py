5238ac16936768450375c90bf095f1b7479cf0d7
Aquiles Carattino <aqui.carattino@gmail.com>
2021-08-13 09:50:41 +0200

    Accepts device number as argument

--- start_piezo_move.py
+++ start_piezo_move.py
@@ -11,7 +11,11 @@ from PiezoMove.main_window import PiezoMoveWindow
 
 os.environ['QT_API'] = 'pyqt5'
 
-device = 5
+device = 0
+if len(sys.argv) > 1:
+    device = int(sys.argv[1])
+
+print(f'Using piezo device {device}')
 
 arduino = ArduinoModel(device=device)
 arduino.initialize()
